d8fe2ee6761c2b3d7cd789241f91c17ebc86560d
Adrian Chirculescu <adrianchirculescu@gmail.com>
2023-01-29 14:58:53 +0200

    loading recipes test

--- src/js/controller.js
+++ src/js/controller.js
@@ -15,13 +15,26 @@ const timeout = function (s) {
 const showRecipe = async function () {
   try {
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=d30a6592-c421-4478-933b-70247b8d74cf'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcf3a'
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=d30a6592-c421-4478-933b-70247b8d74cf'
     );
     const data = await res.json();
 
-    if (!res.ok) throw new Error(`${data.message} (${response.status})`);
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
     console.log(res, data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
   } catch (err) {
     alert(err);
   }
