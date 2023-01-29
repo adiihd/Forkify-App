369bb8b6412bd088f192e829d925abbd6376845f
Adrian Chirculescu <adrianchirculescu@gmail.com>
2023-01-29 16:14:25 +0200

    The MVC Architecture

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -558,7 +558,7 @@ function hmrAccept(bundle, id) {
 
 },{}],"aenu9":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-var _esRegexpFlagsJs = require("core-js/modules/es.regexp.flags.js");
+var _esRegexpFlagsJs = require("core-js/modules/es.regexp.flags.js"); // the mvc architecture
 var _webImmediateJs = require("core-js/modules/web.immediate.js");
 var _iconsSvg = require("url:../img/icons.svg");
 var _iconsSvgDefault = parcelHelpers.interopDefault(_iconsSvg);
--- src/js/controller.js
+++ src/js/controller.js
@@ -161,3 +161,5 @@ const showRecipe = async function () {
 };
 
 ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+
+// the mvc architecture
